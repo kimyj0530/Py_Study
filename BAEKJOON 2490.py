d256669393d25fa0bcdbb2ba9643909817949ed1
김영준 <102145868+kimyj0530@users.noreply.github.com>
2022-10-23 19:36:27 +0900

    Create BAEKJOON 2490.py

---
+++ BAEKJOON 2490.py
@@ -0,0 +1,13 @@
+for i in range(3):
+  a, b, c, d = map(int, input().split())
+  sum = a+b+c+d
+  if sum == 0:
+    print("D")
+  elif sum == 1:
+    print("C")
+  elif sum == 2:
+    print("B")
+  elif sum == 3:
+    print("A")
+  elif sum == 4:
+    print("E")
